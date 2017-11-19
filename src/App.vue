abda8d9f3aac4182ab201cbf5663cad6ba87d4ef
Colm Gallagher <mecolmg@vt.edu>
2017-11-19 16:04:11 -0500

    Base Template for VTHacks single page design
    This commit stages initial development of the VTHacks single page design. It demonstrates use of components for development, and creates the empty templates for: Splash, About/FAQ, Map, and Schedule. This also adds the MLH trust badge. 
    Screenshot: https://i.imgur.com/2Chh9Jx.png

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,13 +11,11 @@ export default {
 </script>
 
 <!-- SCSS is a CSS preprocessor. Check out http://sass-lang.com/ -->
+<!-- This section contains global stylings across all pages. Be
+     careful with adding styles here. -->
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/ScheduleSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="schedule-section">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleSection'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit SCSS to this component only -->
+<!-- SCSS is a CSS preprocessor. Check out http://sass-lang.com/ -->
+<style lang="scss" scoped>
+.schedule-section {
+  width: 100vw;
+  height: 80vh;
+  min-height: 400px;
+  background-color: #555;
+}
+</style>
