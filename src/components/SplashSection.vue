27755f07daba6aeb046621e081bf7d00bf43e571
Colm Gallagher <mecolmg@vt.edu>
2017-11-20 02:11:40 -0500

    Added SCSS Color Variables File
    See description in comments for colors.scss
    
    We'll add to these colors as the designers give us more to work with.

--- src/components/SplashSection.vue
+++ src/components/SplashSection.vue
@@ -18,18 +18,20 @@ export default {
 <!-- Add "scoped" attribute to limit SCSS to this component only -->
 <!-- SCSS is a CSS preprocessor. Check out http://sass-lang.com/ -->
 <style lang="scss" scoped>
+@import '../styles/colors';
 @import url('https://fonts.googleapis.com/css?family=Press+Start+2P');
+
 .splash-section {
   width: 100vw;
   min-height: 400px;
-  background-color: #333;
+  background-color: $background-splash;
 }
 
 .centered-content {
   text-align: center;
   padding: 120px 20px;
   font-family: 'Press Start 2P', cursive;
-  color: white;
+  color: $text-primary;
   font-size: 2em;
   line-height: 1.5em;
 }
