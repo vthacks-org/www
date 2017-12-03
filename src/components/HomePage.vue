38a00766b479accdabc4b92792b3f178178ef81a
Colm Gallagher <mecolmg@vt.edu>
2017-12-03 13:29:41 -0500

    Navigation with top-bar and mobile drawer

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="homepage">
+    <Navigation></Navigation>
     <MLHTrustBadge></MLHTrustBadge>
     <SplashSection></SplashSection>
     <AboutSection></AboutSection>
     <ScheduleSection></ScheduleSection>
     <MapSection></MapSection>
+    <SponsorsSection></SponsorsSection>
   </div>
 </template>
 
 <script>
+import Navigation from './Navigation.vue';
 import MLHTrustBadge from './MLHTrustBadge.vue';
 import SplashSection from './SplashSection.vue';
 import AboutSection from './AboutSection.vue';
 import ScheduleSection from './ScheduleSection.vue';
 import MapSection from './MapSection.vue';
+import SponsorsSection from './SponsorsSection.vue';
 
 export default {
   name: 'HomePage',
   components: {
+    Navigation,
     MLHTrustBadge,
     SplashSection,
     AboutSection,
     ScheduleSection,
     MapSection,
+    SponsorsSection,
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit SCSS to this component only -->
 <!-- SCSS is a CSS preprocessor. Check out http://sass-lang.com/ -->
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
