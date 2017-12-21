cd704be4f28ec6a58fff2652230cc8b54a2bc3c7
Steven Roberts <sarj21@vt.edu>
2017-12-21 00:38:09 -0500

    Added placeholder info to sponsors. Also linked to prospectus
    Waiting on sponsors

--- src/components/SponsorsSection.vue
+++ src/components/SponsorsSection.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="sponsors-section">
+    <h1 class="futuristic blue"> Sponsors </h1>
+    <div class="seperator large pink"></div>
+
+    <div class="info-term">
+      <p>$> <span style="color: blue;">INFO:</span> Sponsors will be published when the list is complete closer to the event</p>
+      <p>$> <span style="color: green;">OPPORUNITY:</span> Want to sponsor us? Check out our prospectus for more info</p>
+      <p>$> cmd open <a href="https://vthacks.com/sponsorship/prospectus" target="_blank"> ( prospectus )</a> | new tab </p>
+    </div>
   </div>
 </template>
 
@@ -12,10 +20,27 @@ export default {
 <!-- Add "scoped" attribute to limit SCSS to this component only -->
 <!-- SCSS is a CSS preprocessor. Check out http://sass-lang.com/ -->
 <style lang="scss" scoped>
+@import '../styles/headers';
+@import '../styles/vars';
+
 #sponsors-section {
   width: 100%;
   height: 80vh;
   min-height: 400px;
-  background-color: #777;
+  background-color: #000;
+  display: flex;
+  flex-direction: column;
 }
+
+.info-term {
+  .tag {
+    color: blue;
+  }
+  font-family: $font-VT232;
+  color: white;
+  word-wrap: normal;
+  align-self: center;
+  padding: 15px;
+}
+
 </style>
