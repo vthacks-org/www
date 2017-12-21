323d321e95fdb5f5318263032b2d2450b00e9ba0
Steven Roberts <sarj21@vt.edu>
2017-12-20 23:56:09 -0500

    Fixed <a> style break
    It was a blue box oops

--- src/components/MLHTrustBadge.vue
+++ src/components/MLHTrustBadge.vue
@@ -1,5 +1,5 @@
 <template>
-  <a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:absolute;right:50px;top:0;width:10%;z-index:10000" href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=white" target="_blank">
+  <a id="mlh-trust-badge" href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=white" target="_blank">
     <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/white.svg" alt="Major League Hacking 2017 Hackathon Season" style="width:100%">
   </a>
 </template>
@@ -11,9 +11,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@media(max-width: 480px) {
-  #mlh-trust-badge {
+
+#mlh-trust-badge {
+  display:block;
+  max-width:100px;
+  min-width:60px;
+  position:absolute;
+  right:50px;
+  top:0;
+  width:10%;
+  z-index:10000;
+  background-color: transparent;
+
+  @media(max-width: 480px) {
     right: 8px !important;
+
   }
+
 }
 </style>
