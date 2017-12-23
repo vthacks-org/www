96d40db906bb5a7973ed5ecbc75164c83f368a11
Steven Roberts <sarj21@vt.edu>
2017-12-22 23:53:02 -0500

    Added Analytics
    Uses vue-analytics

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import smoothScroll from 'vue-smoothscroll';
+import VueAnalytics from 'vue-analytics';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 Vue.use(smoothScroll);
-
+Vue.use(VueAnalytics, {
+  id: 'UA-107929421-1',
+  checkDuplicatedScript: true,
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
