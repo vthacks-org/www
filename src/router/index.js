542410d9a701c2463f5329c70cd754a0fb4e9532
Steven Roberts <sarj21@vt.edu>
2018-02-04 16:21:25 -0500

    Added 404 page
    Routing done by the app.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HomePage from '@/components/HomePage.vue';
+import err404 from '@/components/err404.vue';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HomePage',
       component: HomePage,
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: err404,
     },
   ],
 });
