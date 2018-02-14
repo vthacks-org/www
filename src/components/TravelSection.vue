02a3b15d1bedbdf53437849b2ef8348ccc753e7a
Steven Roberts <sarj21@vt.edu>
2018-02-13 21:09:56 -0500

    Removed bus interest and text for buses

--- src/components/TravelSection.vue
+++ src/components/TravelSection.vue
@@ -5,13 +5,11 @@
       <div id="register-separator" class="seperator yellow large"></div>
 
       <div class="info-term">
-        <p>$> <span style="color: #48d247;">INFO: </span> VTHacks offers a lot of ways to help you get here.
-          We send buses to most schools nearby schools and additionally we offer travel reimbursement to people
+        <p>$> <span style="color: #48d247;">INFO: </span> VTHacks offers a lot of ways to help you get here. We offer travel reimbursement to people
           making the trip themselves or with friends. For questions, email us at <a href="mailto:info@vthacks.com">transportation@vthacks.com</a>.</p>
       </div>
 
       <div class="actions">
-        <a href="/bus-interest" target="_blank" class="register-link medium blue eightbit-btn">Bus Interest &gt;</a>
         <a href="/travel-reimbursement" target="_blank" class="register-link medium blue eightbit-btn">Travel Reimbursement &gt;</a>
       </div>
       <div class="gradient-seperator bottom reversed"></div>
