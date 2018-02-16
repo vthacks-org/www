961b6f4939f28d959c98e0e837ebaad5467b1715
Steven Roberts <sarj21@vt.edu>
2018-02-16 03:38:16 -0500

    Added Socials to footer
    yay social

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -3,6 +3,17 @@
     <div class="code-of-conduct">
        <p> $> <span style="color: #48d247"> INFO: </span> VTHacks adheres to the <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank">( MLH Code of Conduct )</a> </p>
     </div>
+    <div class="socials">
+      <a href="https://www.facebook.com/vthacks" target="_blank">
+        <img src="../assets/8bit_facebook.png">
+      </a>
+      <a href="https://twitter.com/vt_hacks" target="_blank">
+        <img src="../assets/8bit_twitter.png">
+      </a>
+      <a href="https://www.instagram.com/vthacks/" target="_blank">
+        <img src="../assets/8bit_instagram.png">
+      </a>
+    </div>
   </div>
 </template>
 
@@ -23,15 +34,27 @@ export default {
     overflow: hidden;
     display: flex;
     flex-direction: column;
+    align-items: center;
     background: url('../assets/splash-background.png');
   }
 
   .code-of-conduct {
     color: white;
     padding: 20px;
-    align-self: center;
     font-family: $font-VT232;
   }
 
+  .socials {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    a {
+      text-decoration: none;
+      background-color: transparent;
+    }
+    img {
+      margin: 5px 15px 20px 15px;
+    }
+  }
 
 </style>
