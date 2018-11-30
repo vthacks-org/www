f66af37de6d34142eea7b3445feb752060d8aaab
Heather N. Robinson <heather@172.25.84.103>
2018-11-30 11:53:30 -0500

    open registration :)

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -3,17 +3,6 @@
     <div class="code-of-conduct">
        <p> $> <span style="color: #48d247"> INFO: </span> VTHacks adheres to the <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank">( MLH Code of Conduct )</a> </p>
     </div>
-    <div class="socials">
-      <a href="https://www.facebook.com/vthacks" target="_blank">
-        <img src="../assets/8bit_facebook.png">
-      </a>
-      <a href="https://twitter.com/vt_hacks" target="_blank">
-        <img src="../assets/8bit_twitter.png">
-      </a>
-      <a href="https://www.instagram.com/vthacks/" target="_blank">
-        <img src="../assets/8bit_instagram.png">
-      </a>
-    </div>
   </div>
 </template>
 
@@ -42,19 +31,6 @@ export default {
     color: white;
     padding: 20px;
     font-family: $font-VT232;
-  }
-
-  .socials {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    a {
-      text-decoration: none;
-      background-color: transparent;
-    }
-    img {
-      margin: 5px 15px 20px 15px;
-    }
   }
 
 </style>
