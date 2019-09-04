120abed92f33b6ecb6e87b210a2b17598df4b555
Heather Robinson <hnr2800@vt.edu>
2019-09-03 21:54:16 -0400

    add template for collecting emails for pre-registration. still need to fix width for mobile devices

---
+++ src/components/PreRegistrationSection.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="pre-registration">
+		<div class="gradient-seperator"></div>
+		<iframe src="https://cdn.forms-content.sg-form.com/4b4931cd-ceaf-11e9-84e9-1e1be24fe445"/>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@import '../styles/headers';
+@import '../styles/vars';
+@import '../styles/colors';
+
+	iframe {
+		width: 100%;
+		height: 525px;
+		border: none;
+	}
+</style>
