f3bb73f8c3ecc7e2d93d8056f776ced1ba9de7fd
Brian Wieder <brian@4wieders.com>
2020-02-04 21:57:25 -0500

    Added some more events to the schedule

--- src/components/ScheduleSection.vue
+++ src/components/ScheduleSection.vue
@@ -9,51 +9,61 @@
           <img id="neon-sign-on" src="../assets/neon_sign.svg"/>
           <img id="neon-sign-off" src="../assets/neon_sign_off.svg">
       </div>
-      <div style="flex: 1"></div> -->
+      <div style="flex: 1"></div>-->
       <div class="schedules">
         <ul class="schedule">
           <div class="container">
-            <div class="date"> Friday </div>
+            <div class="date">Friday</div>
             <div class="seperator blue small"></div>
             <li v-for="event in events" v-if="event.date == 'Friday'" class="schedule-item">
               <div class="time">
                 {{event.start}}
-                <span v-if="event.end">-<span class="nowrap">{{event.end}}</span></span>
+                <span v-if="event.end">
+                  -
+                  <span class="nowrap">{{event.end}}</span>
+                </span>
               </div>
-              <div class="title"> {{event.title}}</div>
-              <div v-if="event.description" class="description"> {{event.description}}</div>
+              <div class="title">{{event.title}}</div>
+              <div v-if="event.description" class="description">{{event.description}}</div>
             </li>
           </div>
         </ul>
 
-        <ul class="schedule">          <div class="container">
-            <div class="date"> Saturday  </div>
+        <ul class="schedule">
+          <div class="container">
+            <div class="date">Saturday</div>
             <div class="seperator blue small"></div>
-              <li v-for="event in events" v-if="event.date == 'Saturday'" class="schedule-item">
-                <div class="time">
-                  {{event.start}}
-                  <span v-if="event.end">-<span class="nowrap">{{event.end}}</span></span>
-                </div>
-                <div class="title"> {{event.title}}</div>
-                <div v-if="event.description" class="description"> {{event.description}}</div>
-              </li>
+            <li v-for="event in events" v-if="event.date == 'Saturday'" class="schedule-item">
+              <div class="time">
+                {{event.start}}
+                <span v-if="event.end">
+                  -
+                  <span class="nowrap">{{event.end}}</span>
+                </span>
+              </div>
+              <div class="title">{{event.title}}</div>
+              <div v-if="event.description" class="description">{{event.description}}</div>
+            </li>
           </div>
         </ul>
 
         <ul class="schedule">
-        <div class="container">
-          <div class="date"> Sunday   </div>
-          <div class="seperator blue small"></div>
-          <li v-for="event in events" v-if="event.date == 'Sunday'" class="schedule-item">
-            <div class="time">
-              {{event.start}}
-              <span v-if="event.end">-<span class="nowrap">{{event.end}}</span></span>
-            </div>
-            <div class="title"> {{event.title}}</div>
-            <div v-if="event.description" class="description"> {{event.description}}</div>
-          </li>
-        </div>
-      </ul>
+          <div class="container">
+            <div class="date">Sunday</div>
+            <div class="seperator blue small"></div>
+            <li v-for="event in events" v-if="event.date == 'Sunday'" class="schedule-item">
+              <div class="time">
+                {{event.start}}
+                <span v-if="event.end">
+                  -
+                  <span class="nowrap">{{event.end}}</span>
+                </span>
+              </div>
+              <div class="title">{{event.title}}</div>
+              <div v-if="event.description" class="description">{{event.description}}</div>
+            </li>
+          </div>
+        </ul>
       </div>
     </div>
   </div>
@@ -116,6 +126,13 @@ export default {
           end: '9:15 PM',
         },
         {
+          title: 'Learn How to Code',
+          description: 'Room 103A',
+          date: 'Friday',
+          start: '8:15 PM',
+          end: '9:15 PM',
+        },
+        {
           title: 'CoStar Workshop',
           description: 'Room 118C',
           date: 'Friday',
@@ -123,7 +140,14 @@ export default {
           end: '10:30 PM',
         },
         {
-          title: 'Beginner Workshop',
+          title: 'Deploying a ML Model to an App with AI/ML Club',
+          description: 'Room 103A',
+          date: 'Friday',
+          start: '9:30 PM',
+          end: '10:30 PM',
+        },
+        {
+          title: 'Machine Learning "Hello World" with Developer Student Clubs',
           description: 'Room 103A',
           date: 'Friday',
           start: '10:45 PM',
@@ -201,7 +225,7 @@ export default {
           end: '6:00 PM',
         },
         {
-          title: '80\'s Music Bingo',
+          title: "80's Music Bingo",
           description: 'Room 103A',
           date: 'Saturday',
           start: '6:00 PM',
@@ -280,149 +304,174 @@ export default {
 <!-- Add "scoped" attribute to limit SCSS to this component only -->
 <!-- SCSS is a CSS preprocessor. Check out http://sass-lang.com/ -->
 <style lang="scss" scoped>
-  @import '../styles/colors';
-  @import '../styles/vars';
-  @import '../styles/headers';
+@import "../styles/colors";
+@import "../styles/vars";
+@import "../styles/headers";
 
-  #schedule-section {
-    width: 100%;
-    padding: 24px 0;
-    background-color: #000;
-    display: flex;
+#schedule-section {
+  width: 100%;
+  padding: 24px 0;
+  background-color: #000;
+  display: flex;
+  flex-direction: column;
+  font-family: "Inconsolata", monospace;
+}
+
+a {
+  text-decoration: none;
+  color: purple;
+}
+
+.schedule-breakdown {
+  margin-top: 20px;
+  display: flex;
+
+  /* All Mobile Sizes (devices and browser) */
+  @media only screen and (max-width: 767px) {
     flex-direction: column;
-    font-family: 'Inconsolata', monospace;
   }
+  flex-direction: row;
 
-  a {
-    text-decoration: none;
-    color: purple;
-  }
-
-  .schedule-breakdown{
-    margin-top: 20px;
-    display: flex;
-
-    /* All Mobile Sizes (devices and browser) */
+  .date {
+    display: inline-block;
+    color: $neon-blue;
     @media only screen and (max-width: 767px) {
+      font-size: 2em;
+    }
+    font-size: 1.75em;
+    font-family: "Inconsolata", monospace;
+  }
+
+  .schedules {
+    flex-grow: 4;
+    display: flex;
+    justify-content: center;
+    margin: 8px;
+
+    @media only screen and (max-width: 767px) {
+      padding: 20px;
       flex-direction: column;
     }
-    flex-direction: row;
 
-    .date {
-      display: inline-block;
-      color: $neon-blue;
-      @media only screen and (max-width: 767px) {
-        font-size: 2em;
-      }
-      font-size: 1.75em;
-      font-family: 'Inconsolata', monospace;
-    }
-
-    .schedules{
-      flex-grow: 4;
+    ul.schedule {
+      font-family: "Inconsolata", monospace;
+      padding: 0;
+      flex-grow: 1;
       display: flex;
-      justify-content: center;
-      margin: 8px;
+      list-style-type: none;
+      max-width: 25%;
+      margin: 0 auto;
 
       @media only screen and (max-width: 767px) {
-        padding: 20px;
-        flex-direction: column;
-      }
+        margin: 10px; // Things get squished on mobile now
+        max-width: 100%;
 
-      ul.schedule {
-        font-family: 'Inconsolata', monospace;
-        padding: 0;
-        flex-grow: 1;
-        display: flex;
-        list-style-type: none;
-        max-width: 25%;
+        .schedule-item {
+          .time,
+          .title,
+          .description {
+            font-size: 1em;
+          }
+        }
+      }
+      .container {
+        align-items: center;
         margin: 0 auto;
 
         @media only screen and (max-width: 767px) {
-          margin: 10px; // Things get squished on mobile now
-          max-width: 100%;
-
-          .schedule-item {
-            .time, .title, .description {
-              font-size: 1em;
-            }
-          }
-        }
-        .container {
-          align-items: center;
-          margin: 0 auto;
-
-          @media only screen and (max-width: 767px) {
-            margin: 0;
-          }
-        }
-        .schedule-item {
-          padding-bottom: 20px;
-        }
-        .time {
-          font-family: 'Inconsolata', monospace;
-          color: $neon-pink;
-          font-size: 1.2em;
-          line-height: 1.2em;
-        }
-        .title {
-          margin-top: -2px;
-          font-size: 1.2em;
-          color: grey;
-        }
-        .description {
-          margin-top: -2px;
-          font-size: 1.2em;
-          color: $neon-blue;
+          margin: 0;
         }
       }
-    }
-
-  }
-
-  .nowrap {
-    display: inline-block;
-    white-space: nowrap;
-    padding-left: 10px;
-  }
-
-  .neon-sign {
-    flex-grow: 5;
-    position: relative;
-    min-width: 250px;
-    img {
-      width: 100%;
-      height: 100%;
-      align-self: center;
-    }
-    #neon-sign-off {
-      position: absolute;
-      top: 0;
-      animation: animation2 9s infinite;
-    }
-    #neon-sign-on {
-      position: absolute;
-      top: 0;
-      animation: animation1 9s infinite;
+      .schedule-item {
+        padding-bottom: 20px;
+      }
+      .time {
+        font-family: "Inconsolata", monospace;
+        color: $neon-pink;
+        font-size: 1.2em;
+        line-height: 1.2em;
+      }
+      .title {
+        margin-top: -2px;
+        font-size: 1.2em;
+        color: grey;
+      }
+      .description {
+        margin-top: -2px;
+        font-size: 1.2em;
+        color: $neon-blue;
+      }
     }
   }
+}
 
-  @keyframes animation1 {
-    0% { opacity: .95; }
-    95% { opacity: .95; }
-    96% { opacity: 0; }
-    97% { opacity: 0.85;}
-    99% { opacity: 0; }
-    100% { opacity: .95}
-  }
+.nowrap {
+  display: inline-block;
+  white-space: nowrap;
+  padding-left: 10px;
+}
 
-  @keyframes animation2 {
-    0% { opacity: 0; }
-    95% { opacity: 0; }
-    96% { opacity: 0.5; }
-    97% { opacity: 0;}
-    99% { opacity: 0.5; }
-    100% { opacity: 0}
+.neon-sign {
+  flex-grow: 5;
+  position: relative;
+  min-width: 250px;
+  img {
+    width: 100%;
+    height: 100%;
+    align-self: center;
   }
+  #neon-sign-off {
+    position: absolute;
+    top: 0;
+    animation: animation2 9s infinite;
+  }
+  #neon-sign-on {
+    position: absolute;
+    top: 0;
+    animation: animation1 9s infinite;
+  }
+}
+
+@keyframes animation1 {
+  0% {
+    opacity: 0.95;
+  }
+  95% {
+    opacity: 0.95;
+  }
+  96% {
+    opacity: 0;
+  }
+  97% {
+    opacity: 0.85;
+  }
+  99% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0.95;
+  }
+}
+
+@keyframes animation2 {
+  0% {
+    opacity: 0;
+  }
+  95% {
+    opacity: 0;
+  }
+  96% {
+    opacity: 0.5;
+  }
+  97% {
+    opacity: 0;
+  }
+  99% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0;
+  }
+}
 </style>
 
