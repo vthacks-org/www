72d789e666a79959838279afb5f16f82ebd9f3bd
Joe Conwell <jmc529@vt.edu>
2021-01-26 19:40:01 -0500

    Updated meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'name', name: 'name', content: 'VTHacks 8' },
+      { hid: 'description', name: 'description', content: 'A landing page for VTHacks, a MLH hackathon organized by Virginia Tech students.' },
+      { hid: 'image', name: 'image', content: '~static/sitemeta.svg' },
+      { hid: 'url', name: 'url', content: 'https://vthacks.com' },
     ],
     link: [{ rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' }],
   },
