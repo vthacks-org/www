9c2d0cb5c6862a3d6b2f4cc43f40994f89204ae6
GryphNdor <griffin@ogura-family.com>
2022-09-11 21:42:14 -0400

    initial commit

---
+++ apps/frontend/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <h1>Yo</h1>
+    </>
+  );
+}
+
+export default App;
