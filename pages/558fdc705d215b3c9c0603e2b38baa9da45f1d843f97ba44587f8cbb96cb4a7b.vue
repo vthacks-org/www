bf8f116a6346cbc9113840dd67c35ec2cafc1b8c
GryphNdor <griffin@ogura-family.com>
2022-11-06 13:57:27 -0500

    vt scavenger puzzle

---
+++ pages/558fdc705d215b3c9c0603e2b38baa9da45f1d843f97ba44587f8cbb96cb4a7b.vue
@@ -0,0 +1,37 @@
+---
+navigation: false
+---
+
+<template>
+  <div class="container">
+    <div>
+      <h1>Puzzle Two</h1>
+      <h3>{{ getUrl() }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PuzzleOne',
+  data() {
+    return {
+      showPage: false,
+    }
+  },
+  methods: {
+    getUrl() {
+      return window.location.pathname
+    },
+  },
+}
+</script>
+
+<style type="text/css">
+.container {
+  display: grid;
+  height: 100vh;
+  place-items: center;
+  color: black;
+}
+</style>
