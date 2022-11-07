a47e61d8fec6ee9ff10b532ad236fe7d93bac874
GryphNdor <griffin@ogura-family.com>
2022-11-07 15:42:28 -0500

    added checkin link

---
+++ pages/checkin.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div>
+      <h3>{{ url }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckIn',
+  data() {
+    return {
+      url: this.getUrl(),
+    }
+  },
+  mounted() {
+    this.url = window.location.search
+  },
+  methods: {
+    getUrl() {
+      return this.url
+    },
+  },
+}
+</script>
+
+<style type="text/css">
+.container {
+  display: grid;
+  height: 100vh;
+  place-items: center;
+  color: black;
+}
+</style>
