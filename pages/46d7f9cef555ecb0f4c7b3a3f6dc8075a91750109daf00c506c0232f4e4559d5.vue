f2bbf72d3d2c2759c19a2a442fb34863bb15f37b
GryphNdor <griffin@ogura-family.com>
2022-11-10 13:19:01 -0500

    added more puzzles

---
+++ pages/46d7f9cef555ecb0f4c7b3a3f6dc8075a91750109daf00c506c0232f4e4559d5.vue
@@ -0,0 +1,45 @@
+---
+navigation: false
+---
+
+<template>
+  <div class="container">
+    <div
+      class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
+    >
+      <h1>Puzzle Two</h1>
+      <h3 style="text-align: center">
+        I'm sure that puzzle was a little too easy for you. You're almost
+        halfway there! But, don't get too antsy. Hackers should possess a wide
+        range of skills. Let's test a different skill. Can you find the hidden
+        message in this image?
+      </h3>
+      <hr />
+      <img src="~/static/spaceship.jpeg" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PuzzleTwo',
+  data() {
+    return {
+      showPage: false,
+    }
+  },
+}
+</script>
+
+<style type="text/css">
+.container {
+  display: grid;
+  height: 100vh;
+  place-items: center;
+  color: black;
+}
+
+.is-flex {
+  width: 60%;
+}
+</style>
---
+++ pages/bdf38df190adf1d87efad258b5761f539f26204bef6ee6271a42898ab0608068.vue
@@ -0,0 +1,59 @@
+---
+navigation: false
+---
+
+<template>
+  <div class="container">
+    <div
+      class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
+    >
+      <h1>Puzzle Four</h1>
+      <h3>
+        I created an encryption. I call it the HOMERIFY ENCRYPTOR 9000! I
+        encrypted an email using my encryptor, but I don’t really know how to
+        decrypt it. Can you help me reverse engineer my code and decrypt my
+        email?
+      </h3>
+      <hr />
+      <h3>
+        Encrypted Email: OMG!! QAO XYDE EIHNXH FVGKVR HCXGPYHW. QAO OQY VOBNKR
+        FUG RTA SHNQL! USIO NC QCQC AAHP QAO'FU HKA A VABMC, VCBU'C A EKSPQE
+        UGKXGW FUG RTA GSDF: XRTV JWAEGTM OQY BVKFRNWXBV HYLA A FEGDH IHPBO BTVT
+        PCKTZ QMWI ER PCKTZ FRQ ER RTA GEUE? OTHU'C PSMF GSDF! TEAKC
+      </h3>
+      <hr />
+      <h3>
+        <a
+          href="https://drive.google.com/uc?export=download&id=1UiEkAFXQLm6FXaKuzqN1vR40IEXlhrEX"
+          download
+        >
+          Python File
+        </a>
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PuzzleFour',
+  data() {
+    return {
+      showPage: false,
+    }
+  },
+}
+</script>
+
+<style type="text/css">
+.container {
+  display: grid;
+  height: 100vh;
+  place-items: center;
+  color: black;
+}
+
+.is-flex {
+  width: 60%;
+}
+</style>
